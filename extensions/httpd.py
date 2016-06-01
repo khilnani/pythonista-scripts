48906b3416ec71d78e769c9a71bb41bb0c37c9ac
khilnani <nik@khilnani.org>
2016-06-01 01:15:14 -0400

    added

---
+++ extensions/httpd.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+"""
+Description:
+- Starts up a simple HTTP server with the open file's directory as the Document Root.
+
+Instructions:
+- Add the script to the Pythonista Actions Shortcut menu.
+- Open any file in the directory you want the server to start, and run the script.
+"""
+import SimpleHTTPServer
+import BaseHTTPServer
+import socket
+import os
+
+PORT = 8000
+
+try:
+    import editor
+    file = editor.get_path()
+    dir = os.path.sep.join( file.split( os.path.sep )[:-1] )
+    print('Using dir: %s' % dir)
+    os.chdir( dir )
+except ImportError:
+    print('Not run in iOS Pythonista, will use current working directory')
+
+try:
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = BaseHTTPServer.HTTPServer(('', PORT), Handler)
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8',80))
+    ip = s.getsockname()[0]
+    s.close()
+    
+    print("Server started at: http://%s:%s" % (ip, PORT))
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("Shutting down ...")
+    httpd.shutdown()
+    httpd.server_close()
+    print("Done.")
+    
